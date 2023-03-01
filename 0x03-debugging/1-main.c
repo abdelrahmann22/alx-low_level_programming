8aeab6bae9ead4fc5e292cf801fa8e01d7f8c64d
abdelrahmann22 <boudyali22@gmail.com>
2023-03-01 04:53:55 -0800

    infinte loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,13 +13,11 @@ int main(void)
 
         i = 0;
 	/*
-	 * Don't Delete it
-	 * */
         while (i < 10)
         {
                 putchar(i);
         }
-
+	*/
         printf("Infinite loop avoided! \\o/\n");
 
         return (0);
