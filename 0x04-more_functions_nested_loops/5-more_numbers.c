f6d6e1bb0b91439470cd3059ae5e5dc2e1008028
abdelrahmann22 <boudyali22@gmail.com>
2023-03-05 18:23:23 -0800

    More numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,10 @@ void more_numbers(void)
 	{
 		for (b = 0; b <= 14; b++)
 		{
-			_putchar(b + '0');
+			if (b >= 10)
+				_putchar((b / 10) + 48);
+			_putchar((b % 10) + 48);
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
