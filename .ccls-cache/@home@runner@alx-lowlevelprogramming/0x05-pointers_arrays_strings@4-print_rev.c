5be751244e964444243542a3fd23ffcfee0ab116
Abdelrahman <31183876+abdelrahmann22@users.noreply.github.com>
2023-03-17 02:28:07 +0000

    Reverse

---
+++ .ccls-cache/@home@runner@alx-lowlevelprogramming/0x05-pointers_arrays_strings@4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: input string.
+ * Return: no return.
+ */
+void print_rev(char *s)
+{
+  if (*s)
+  {
+    print_rev(s + 1);
+    _putchar(*s);
+  }
+}
