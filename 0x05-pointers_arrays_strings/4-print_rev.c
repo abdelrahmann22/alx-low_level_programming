377449f1b6017f6e9a73d0b1d98edfbe959b68ca
abdelrahmann22 <boudyali22@gmail.com>
2023-03-16 19:46:59 -0700

    Reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,9 +6,16 @@
  */
 void print_rev(char *s)
 {
-  if (*s)
-  {
-    print_rev(s + 1);
-    _putchar(*s);
-  }
+	int count = 0;
+	
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		count++;
+	}
+
+	for (count--; count >=0; count--)
+		_putchar(s[count]);
+	_putchar('\n');
 }
