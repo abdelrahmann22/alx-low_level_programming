691d9a5c8830ef3534b7c79e71e01fe3527af720
abdelrahmann22 <boudyali22@gmail.com>
2023-03-16 18:01:36 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
+ */
+int _strlen(char *s){
+	int counter, i = 0;
+	
+	while (*(s + i) != '\0')
+	{
+		counter += 1;
+	}
+	return (counter);
+}
