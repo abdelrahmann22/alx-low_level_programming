915217acd57a130d3e8ff330608b05adb2991e32
Abdelrahman <31183876+abdelrahmann22@users.noreply.github.com>
2023-03-19 07:07:00 +0200

    Puts Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,14 +5,13 @@
  *
  * Return: void
  */
-
 void _puts_recursion(char *s)
 {
-    if (*s != '\0')
-    {   
-        _putchar(*s);
-        _puts_recursion(s + 1);    
-    }
-    else
-        _putchar('\n');
+	if (*s != '\0')
+	{   
+		_putchar(*s);
+		_puts_recursion(s + 1);    
+	}
+	else
+		_putchar('\n');
 }
