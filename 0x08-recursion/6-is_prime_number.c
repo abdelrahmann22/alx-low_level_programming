e53a505ff95311daa8767334d070a878ebd7eeba
Abdelrahman <31183876+abdelrahmann22@users.noreply.github.com>
2023-03-20 04:31:16 +0200

    Prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -15,7 +15,7 @@ int mod_prime(int n, int c)
 		else
 			return (0);
 	}
-	return (is_prime(n, c + 1));
+	return (mod_prime(n, c + 1));
 }
 /**
  * is_prime_number - detects if an input number is a prime number.
@@ -30,5 +30,5 @@ int is_prime_number(int n)
 		return (0);
 	if (n == 1)
 		return (0);
-	return (is_prime(n, 2));
+	return (mod_prime(n, 2));
 }
