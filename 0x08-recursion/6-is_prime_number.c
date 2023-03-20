55cd97a64634d368d45aa14decd21e210b983bc0
Abdelrahman <31183876+abdelrahmann22@users.noreply.github.com>
2023-03-20 04:32:40 +0200

    Orime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * is_prime - detects if an input number is a prime number.
+ * mod_prime - detects if an input number is a prime number.
  * @n: input number.
  * @c: iterator.
  * Return: 1 if n is a prime number. 0 if n is not a prime number.
