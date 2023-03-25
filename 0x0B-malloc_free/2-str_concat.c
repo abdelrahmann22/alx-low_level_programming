d3b06c94c315cff4a7459656dfd3f523a4fe0cc6
Abdelrahman <31183876+abdelrahmann22@users.noreply.github.com>
2023-03-25 08:31:34 +0200

    strconcat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,9 +10,8 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int n = 0 , x =0 , i , j;
+	int n = 0, x =0, i, j;
 	char *str;
-	
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
@@ -22,22 +21,17 @@ char *str_concat(char *s1, char *s2)
 		n++;
 	while (s2[x] != '\0')
 		x++;
-	
 	str = (char *)malloc(sizeof(char) * (n + x + 1));
 
-	if (str ==  NULL)
+	if (str == NULL)
 	{
-		free(str);
-		return(NULL);
+		free (str);
+		return (NULL);
 	}
 	for (i = 0; i < n; i++)
-	{
 		str[i] = s1[i];
-	}
 	for (j = 0; j < x; j++)
-	{
 		str[i++] = s2[j];
-	}
 
 	return (str);
 }
